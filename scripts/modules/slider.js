be72a6c6054a40014591a1eefc6b895f017534af
niksedov24 <nik.sedov890@gmail.com>
2025-11-12 17:44:10 +0400

    fix adaptiv

--- scripts/modules/slider.js
+++ scripts/modules/slider.js
@@ -1,4 +1,13 @@
 new Swiper(".swiper", {
   spaceBetween: 25,
   slidesPerView: 1.5,
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+    },
+
+    700: {
+      slidesPerView: 1.5,
+    },
+  },
 });
